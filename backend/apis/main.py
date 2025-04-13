2f9adefe8f2ba0fa8ae30cd48a654b687fe26e97
Sukanya Sahoo <sukanyasahoo41455@gmail.com>
2025-04-13 13:34:00 -0500

    added bm25 api

---
+++ backend/apis/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from .utils import BM25Helper
+
+
+app = FastAPI()
+bmobj = BM25Helper()
+bmobj.load_data()
+bmobj.store_corpus()
+
+class Item(BaseModel):
+    user: str
+    query: str 
+
+@app.post("/backend/bm25")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.user is not None:
+        query = item.query
+        top_n = bmobj.get_ranks(query)
+        item_dict.update({"ranked_docs": top_n})
+    return item_dict
