8000febb505437788ac461918e9cbdee9ccada13
gauri-1 <108271564+gauri-1@users.noreply.github.com>
2025-04-13 19:06:37 -0500

    Update main.py

--- backend/apis/main.py
+++ backend/apis/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .utils import BM25Helper
-
-
+ 
+import pandas as pd
+ 
+ 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -12,16 +14,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
+ 
+ 
 bmobj = BM25Helper()
 bmobj.load_data()
 bmobj.store_corpus()
-
+ 
 class Item(BaseModel):
     user: str
-    query: str 
-
+    query: str
+ 
 @app.post("/backend/bm25")
 async def create_item(item: Item):
     item_dict = item.dict()
@@ -30,3 +32,10 @@ async def create_item(item: Item):
         top_n = bmobj.get_ranks(query)
         item_dict.update({"ranked_docs": top_n})
     return item_dict
+ 
+@app.get("/backend/orgs-list")
+def get_organizations():
+    csv_path = "backend/data/Organisations_Master.csv"
+    df = pd.read_csv(csv_path)
+    data = df.fillna('').to_dict(orient="records")
+    return data
